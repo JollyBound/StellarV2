aec8f9adf18a6becf2315e5dd3fb88998446e5b6
APCHI <72634974+JollyBound@users.noreply.github.com>
2023-08-11 12:05:18 +0300

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,13 @@
+export default {
+  //Site display name
+  SITE_TITLE: "StellarTV",
+
+  //Put your discord link here or null to disable
+  SITE_DISCORD: "https://discord.gg/SAS6Jv6ZQQ",
+  
+  //True or false to show or hide site credit in footer (You should leave enabled 😉)
+  SITE_CREDITS: false,
+
+  //Do not change or site will break!
+  RIPPER_API: "https://api.riptv.net"
+}
